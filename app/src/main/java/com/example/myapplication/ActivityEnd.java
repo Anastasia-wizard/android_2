5b1e3727b0ebae6413283d05143da4def72a92cf
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-02 17:13:35 +0300

    Update ActivityEnd.java

--- app/src/main/java/com/example/myapplication/ActivityEnd.java
+++ app/src/main/java/com/example/myapplication/ActivityEnd.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import static com.example.myapplication.R.id.resultDesk;
+
 
 public class ActivityEnd extends AppCompatActivity {
     private TextView resultDesk;
@@ -21,16 +23,16 @@ public class ActivityEnd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
 
-        //ResultString = getIntent().getStringExtra("answer");
+
+        ResultString = getIntent().getStringExtra("answer");
         resultDesk = findViewById(R.id.resultDesk);
 
 
+        ResultString = getIntent().getStringExtra("answer"); // принимать тоже надо по ключу answer
+        resultDesk = findViewById(R.id.resultDesk);
+        resultDesk.setText(ResultString);
+
+
     }
 
-    public TextView getResultDesk() {
-        Intent intent = new Intent(ActivityEnd.this, MainActivity.class);
-        intent.getStringArrayListExtra("finish");
-        startActivity(intent);
-        return resultDesk;
-    }
 }
